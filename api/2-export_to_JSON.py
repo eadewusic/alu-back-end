ae454a8c57d368859afd9b25b520b28221aece84
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-11-05 21:41:01 +0200

    Update 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 Using what you did in the task #0,
 extend your Python script to export
@@ -7,9 +8,17 @@ data in the JSON format.
 Requirements:
 
 Records all tasks that are owned by this employee
-Format must be: { "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
+
+Format must be:
+{ "USER_ID": [{"task": "TASK_TITLE", 
+"completed": TASK_COMPLETED_STATUS,
+"username": "USERNAME"},
+{"task": "TASK_TITLE",
+"completed": TASK_COMPLETED_STATUS,
+"username": "USERNAME"}, ... ]}
 File name must be: USER_ID.json
 """
+
 if __name__ == "__main__":
     import json
     import requests
