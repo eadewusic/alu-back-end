7bbf78cc75707fb0281c896f3ed9d4ba712df5cc
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-11-05 20:56:27 +0200

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """
-Using what you did in the task #0,
-extend your Python script to export
-data in the CSV format.
+This script fetches and displays an employee's TODO list and exports the data in CSV format.
 
-Requirements:
-
-Records all tasks that are owned by this employee
-Format must be:
-"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-File name must be: USER_ID.csv
+Usage:
+    ./1-export_to_CSV.py <employee_id>
 """
+import csv
+import requests
+from sys import argv
+
 if __name__ == "__main__":
     import csv
     import requests
